9029db4caf9664f1762199fa5ae3084263457d40
Vladislav Popov <lekan.lekan@gmail.com>
2021-06-18 22:39:45 +0300

    append method

--- main.py
+++ main.py
@@ -6,3 +6,14 @@ class Node:
 class LinkedList:
     def __init__(self) -> None:
         self.head = None
+
+    def append(self, data):
+        new_node = Node(data)
+        if self.head == None:
+            self.head = new_node
+            return
+        last_node = self.head           
+        while last_node.next:
+            last_node = last_node.next
+        last_node.next = new_node
+        
